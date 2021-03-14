2f7677c9384bae21d6f559faf8dbfec56dd58564
Ailton Jr <aosilvajr@gmail.com>
2021-03-14 17:57:16 -0300

    refactor: move request creation to a factory method

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -2,7 +2,14 @@ import { AccountModel } from '../../domain/models/account'
 import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token '
 import { AccessDeniedError } from '../errors'
 import { forbidden } from '../helpers/http/http-helper'
+import { HttpRequest } from '../protocols'
 import { AuthMiddleware } from './auth-middleware'
+
+const makeFakeRequest = (): HttpRequest => ({
+  headers: {
+    'x-access-token': 'any_token'
+  }
+})
 
 const makeFakeAccount = (): AccountModel => ({
   id: 'valid_id',
@@ -47,11 +54,7 @@ describe('Auth Middleware', () => {
     const { sut, loadAccountByTokenStub } = makeSut()
     const loadSpy = jest.spyOn(loadAccountByTokenStub, 'load')
 
-    await sut.handle({
-      headers: {
-        'x-access-token': 'any_token'
-      }
-    })
+    await sut.handle(makeFakeRequest())
 
     expect(loadSpy).toHaveBeenCalledWith('any_token')
   })
