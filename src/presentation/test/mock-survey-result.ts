f3600ac79d79922d646ff4f1e38c6307fedee087
Ailton Jr <aosilvajr@gmail.com>
2021-03-21 20:03:50 -0300

    feat: ensure LoadSurveyResultController calls LoadSurveyResult with correct value

---
+++ src/presentation/test/mock-survey-result.ts
@@ -0,0 +1,13 @@
+import { SurveyResultModel } from '@/domain/models/survey-result'
+import { mockSurveyResultModel } from '@/domain/test'
+import { LoadSurveyResult } from '@/domain/usecases/survey-result/load-survey-result'
+
+export const mockLoadSurveyResult = (): LoadSurveyResult => {
+  class LoadSurveyResultStub implements LoadSurveyResult {
+    async load (surveyId: string): Promise<SurveyResultModel> {
+      return await Promise.resolve(mockSurveyResultModel())
+    }
+  }
+
+  return new LoadSurveyResultStub()
+}
