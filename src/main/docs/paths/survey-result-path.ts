9364014bc402a0faef12b1d9bd6552f3df5cb236
Ailton Jr <aosilvajr@gmail.com>
2021-03-21 20:28:48 -0300

    docs: add load survey result path

--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -43,5 +43,42 @@ export const surveyResultPath = {
         $ref: '#/components/unauthorized'
       }
     }
+  },
+  get: {
+    security: [{
+      apiKeyAuth: []
+    }],
+    tags: ['Enquete'],
+    summary: 'API para consultar o resultado de ma enquete',
+    description: 'Essa rota só pode ser executada por **usuário autenticado**',
+    parameters: [{
+      in: 'path',
+      name: 'surveyId',
+      required: true,
+      schema: {
+        type: 'string'
+      }
+    }],
+    responses: {
+      200: {
+        description: 'Success',
+        content: {
+          'appplication/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult'
+            }
+          }
+        }
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      },
+      500: {
+        $ref: '#/components/unauthorized'
+      }
+    }
   }
 }
